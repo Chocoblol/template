6761b2110db046e36e0f503f547eab1e2c9a09cd
DESKTOP-5LJV2H7\Aleksei <exponat90@gmail.com>
2018-08-07 23:15:18 +0300

    скрипт работает,но нужна калибровка, т.к. блоки стоят не ровно на экране добавлены комментарии

--- scrolling.js
+++ scrolling.js
@@ -1,52 +1,48 @@
 window.addEventListener('DOMContentLoaded', () =>{
-	//Получем сылки с навигационного меню
-	let navList = document.querySelectorAll('li > a');
+	
 
-	// Получаем элементы на которых идет ссылка с навигационного меню
-	let AnchorsElements = document.querySelectorAll('li > a').href;
-
-	let scroll = false;
+	let scroll = false; //техническая переменная
+	let i;				//техническая переменная
 
 	// ПЛАВНАЯ ПРОКРУТКА ПРИ НАЖАТИИ ССЫЛОК В НАВИГАЦИОННОМ МЕНЮ
-	let navMenu = document.querySelector('.container');
+	let navMenu = document.querySelector('.container'); //получаем навигационное меню
 
 	navMenu.addEventListener('click', function(event){
-		let scrollTarget, link, element;
+		let link, element;
 		let target = event.target;
 		if (target && target.matches('a')){
-			event.preventDefault();
+			event.preventDefault(); // отменяем стандартное поведение элемента
 			scroll = true;
-			link = target.getAttribute('href');
+			link = target.getAttribute('href'); // получаем id ссылки из href
 			//console.log(link);
-			element = document.querySelector(link);	
+			element = document.querySelector(link);	// получаем элемент на который идет ссылка
 			//console.log(element);		
-			target = element.offsetTop;
-			console.log(target);
-			i = 0;
+			let distance = element.offsetTop; // получаем расстояние до элемента
+			//console.log(target);
+			i = window.pageYOffset; // получаем текушую позицию
+			console.log(i);
 
-			if (target > 0 && scroll == true){
+			if (i < distance && scroll == true){
 				function scrollDown(){
-					if (i < target){
+					if (i < distance){
 						window.scrollTo(0, i);
 						i = i + 50;
 						let timer = setTimeout(scrollDown, 30);
 					} else {
-						clearTimeout(timer);
-						scroll = false;
-						return i 
+						clearTimeout(timer);//останавливаем интервал
+						scroll = false;		// отключаем скролл	
 					};
 				};
 				scrollDown();
-			} if (target < 0 && scroll == true){
+			} if (i > distance && scroll == true){
 				function scrollUp(){
-					if (i < target){
+					if (i > distance){
 						window.scrollTo(0, i);
 						i = i - 50;
-						let timer = setTimeout(scrollDown, 30)
+						let timer = setTimeout(scrollUp, 30)
 					} else {
-						clearTimeout(timer);
-						scroll = false;
-						return i 
+						clearTimeout(timer);//останавливаем интервал
+						scroll = false;		// отключаем скролл	
 					}
 				};
 				scrollUp();
