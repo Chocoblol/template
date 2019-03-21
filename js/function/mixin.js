8b78fb08bc8f74eb3e7d71daaa8dc9cb96e2277d
Aleksei Zhuravlev <exponat90@gmail.com>
2019-03-21 22:57:53 +0300

    добавлен файл для примера мемоизации

--- js/function/mixin.js
+++ js/function/mixin.js
@@ -20,3 +20,39 @@ const mixinCalculateCost = obj => {
 [g1,g2,g3].forEach((i) => {
 	i.calculateCost()
 });
+
+// еще пример
+
+// const extend = (obj, mixin) => {
+// 	for (let key in mixin) {
+// 		if(mixin.hasOwnProperty(key)) {
+// 			obj[key] = mixin[key];
+// 		};
+// 	};
+// 	return obj;
+// };
+
+const extend = (obj, mixin) => {
+	Object.keys(mixin).forEach( key => obj[key] = mixin[key]);
+	return obj;
+};
+
+let person = {
+	name: 'Marcus Aurelius',
+	city: 'Rome',
+	born: '121-04-26',
+};
+
+const mix1 = {
+	toString() {
+		return `${this.name} was born at ${this.born}`
+	},
+	age() {
+		const year = new Date().getFullYear();
+		const born = new Date(this.born).getFullYear();
+		return year - born;
+		return
+	}, 
+};
+
+let mixx = extend(mix1, mixin);
