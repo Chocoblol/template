92bf37ab13afa0f4869cb0b90fa3c5859bb027e4
Aleksei Zhuravlev <exponat90@gmail.com>
2019-03-21 21:41:44 +0300

    добавлен пример с for await и abortcontroller

--- js/API/fetch.js
+++ js/API/fetch.js
@@ -2,4 +2,30 @@
 
 // примеры с fetch
 
+const controller = new AbortController();
+const signal = controller.signal; 
 
+const config = {
+	// body: JSON.stringify(data),
+	headers: new Headers({
+		'Content-Type': 'Application/json',
+	}),
+	signal: signal,
+};
+
+async function getData(data) {
+	let res =  await fetch('http://jsonplaceholder.typicode.com/photos', config)
+	// .then( data => {controller.abort(); return data})
+	.then( res => res.json())
+	.then( data => {
+		// console.log(data);
+		(async() => {
+			for await (let key of data) {
+				console.log(key)
+			};
+		})();
+	});
+	if (!res.ok) console.error('fail loading');
+};
+
+getData();
