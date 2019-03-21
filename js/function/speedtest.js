0d21d6540ad73dae66b35d15c59acb4f13ad8f9f
Aleksei Zhuravlev <exponat90@gmail.com>
2019-03-21 23:47:14 +0300

    перенесены функции

--- js/function/speedtest.js
+++ js/function/speedtest.js
@@ -2,10 +2,10 @@
 
 const LOOP_COUNT = 10000;
 
-const speedTest = (name, fn, args, LOOP_COUNT) => {
+const speedTest = (name, fn, args, count) => {
 	const tmp = [];
 	const start = new Date().getTime();
-	for (let i = 0 ; i < LOOP_COUNT; i++) {
+	for (let i = 0; i < count; i++) {
 		tmp.push(fn(...args));
 	};
 	const time = new Date().getTime() - start;
