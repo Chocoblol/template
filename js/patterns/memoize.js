c9b8e453b800f829103c5771216527be572151c4
Aleksei Zhuravlev <exponat90@gmail.com>
2019-03-22 03:01:50 +0300

    добавлен пример мемоизации для функций с колбэком

--- js/patterns/memoize.js
+++ js/patterns/memoize.js
@@ -27,7 +27,7 @@ const memoize = (fn, length) => {
 		let key = generateKey(...args);
 		if (cache.has(key)) return cache.get(key);
 		let res = fn(...args);
-		if (cache.size() >= length) {
+		if (cache.size >= length) {
 			let firstKey = cache.keys().next().value;
 			console.log(`delete key ${firstKey}`);
 			cache.delete(key);
@@ -36,3 +36,61 @@ const memoize = (fn, length) => {
 		return res;
 	};
 };
+
+
+/// пример мемоизации с колбэками
+const fs = require('fs');
+
+// Production implementation from Metasync library
+// See: https://github.com/metarhia/metasync
+
+function Memoized() {}
+
+const memoize = fn => {
+  const cache = new Map();
+
+  const memoized = function(...args) {
+    const callback = args.pop();
+    const key = args[0];
+    const record = cache.get(key);
+    if (record) {
+      console.log('Read from cache');
+      callback(record.err, record.data);
+      return;
+    }
+    fn(...args, (err, data) => {
+      cache.set(key, { err, data });
+      callback(err, data);
+    });
+  };
+
+  const fields = {
+    cache,
+    timeout: 0,
+    limit: 0,
+    size: 0,
+    maxSize: 0,
+  };
+
+  Object.setPrototypeOf(memoized, Memoized.prototype);
+  return Object.assign(memoized, fields);
+};
+
+Memoized.prototype.clear = function() {
+  this.cache.clear();
+};
+
+// Usage
+
+fs.readFile = memoize(fs.readFile);
+
+fs.readFile('6-metasync.js', 'utf8', (err, data) => {
+  console.log('data length:', data.length);
+  fs.readFile('6-metasync.js', 'utf8', (err, data) => {
+    console.log('data length:', data.length);
+    fs.readFile.clear();
+    fs.readFile('6-metasync.js', 'utf8', (err, data) => {
+      console.log('data length:', data.length);
+    });
+  });
+});
